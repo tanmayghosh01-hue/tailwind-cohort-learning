1e7903a775e2a43dc3158a49c4982ab25af30eb2
tanmayghosh01-hue <tanmayghosh1st@gmail.com>
2024-12-11 00:02:58 +0530

    modified sidebar and created button

--- tailwind-assignment-two/src/components/SideBar.jsx
+++ tailwind-assignment-two/src/components/SideBar.jsx
@@ -9,7 +9,16 @@ const SideBar = () => {
   return (
     <div className="bg-slate-100 h-screen w-80">
       <div>
-        <div className="mt-[10px] mb-[10px] pl-[15px]">SideBar Header</div>
+        <div className="mt-[10px] mb-[10px] pl-[15px] pr-[35px]">
+          <div className="flex justify-between items-center">
+              <div className="flex items-center bg-cyan-900 text-zinc-100 w-32 h-8 rounded justify-center">
+                <PiMonitorPlayFill className="text-cyan-400" /> <span className="ml-1">Webinar.</span>gg
+              </div>
+              <div className="text-xl mt-[3px]">
+                <PiMonitorPlayFill />
+              </div>
+            </div>
+        </div>
         <div className="flex flex-col mt-[20px] gap-[20px] pl-[15px] pr-[35px]">
           <div className="flex justify-between items-center">
             <div>Home</div>
@@ -17,10 +26,30 @@ const SideBar = () => {
               <FaHome />
             </div>
           </div>
-          <div>Webinars</div>
-          <div>Billing</div>
-          <div>User Management</div>
-          <div>Settings</div>
+          <div className="flex justify-between items-center">
+            <div>Webinars</div>
+            <div className="text-xl mt-[3px]">
+              <FaPeopleGroup />
+            </div>
+          </div>
+          <div className="flex justify-between items-center">
+            <div>Billing</div>
+            <div className="text-xl mt-[3px]">
+              <FaRegCreditCard />
+            </div>
+          </div>
+          <div className="flex justify-between items-center">
+            <div>User Management</div>
+            <div className="text-xl mt-[3px]">
+              <ImProfile />
+            </div>
+          </div>
+          <div className="flex justify-between items-center">
+            <div>Settings</div>
+            <div className="text-xl mt-[3px]">
+              <IoSettings />
+            </div>
+          </div>
         </div>
       </div>
     </div>
