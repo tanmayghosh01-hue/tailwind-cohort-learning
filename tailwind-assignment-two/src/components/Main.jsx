1dc5591453e9cb11f7f747600153deb14466f01c
tanmayghosh01-hue <tanmayghosh1st@gmail.com>
2024-12-13 23:37:03 +0530

    refined layout for main section

--- tailwind-assignment-two/src/components/Main.jsx
+++ tailwind-assignment-two/src/components/Main.jsx
@@ -4,15 +4,17 @@ const Main = () => {
       <div>
         background image
       </div>
-      <div>
-        Greeting message
-      </div>
-      <div className="flex gap-8 ml-6">
-        <div className="w-[200px] border border-l-1 border-indigo-400"> 
-          Profile Section
+      <div className="flex gap-8 ml-6 mt-[80px]">
+        <div className="w-[200px] border border-l-1 border-indigo-400 relative"> 
+          <div className="absolute border border-l-1 border-fuchsia-600 top-[-80px]">
+            Profile Section
+          </div>
         </div>
-        <div className="w-[400px] border border-l-1 border-indigo-400">
+        <div className="w-[400px] border border-l-1 border-indigo-400 relative">
           calender Section
+          <div className="absolute top-[-80px]">
+            Good Morning Prabhleen
+          </div>
         </div>
         <div className="w-[230px] border border-l-1 border-indigo-400">
           Join Section
