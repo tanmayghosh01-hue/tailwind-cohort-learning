c9c1ad4adaffeb2ae31938dee5747e16637a40b4
tanmayghosh01-hue <tanmayghosh1st@gmail.com>
2024-12-14 17:29:12 +0530

    container alignment and height changes

--- tailwind-assignment-two/src/components/Main.jsx
+++ tailwind-assignment-two/src/components/Main.jsx
@@ -5,12 +5,12 @@ const Main = () => {
         background image
       </div>
       <div className="flex gap-8 ml-6 mt-[80px]">
-        <div className="w-[200px] border border-l-1 border-indigo-400 relative"> 
-          <div className="absolute border border-l-1 border-fuchsia-600 top-[-80px]">
+        <div className="w-[200px] border-indigo-400 relative"> 
+          <div className="w-[200px] h-[250px] absolute border border-l-1 border-fuchsia-600 top-[-125px]">
             Profile Section
           </div>
         </div>
-        <div className="w-[400px] border border-l-1 border-indigo-400 relative">
+        <div className="w-[400px] h-[250px] border border-l-1 border-indigo-400 relative">
           calender Section
           <div className="absolute top-[-80px]">
             Good Morning Prabhleen
